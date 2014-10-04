131b23013f29f1ff182dfcc1cbb0ca6cdad07f8c
Fabio Ticconi <fabio.ticconi@gmail.com>
2014-10-05 00:54:59 +0100

    Using "loop fusion" to improve performance (thanks @andytill for the tip). Should be fine in a single loop until I put in interactive forces between particles.
    Also fixed some minor glitches and improved the fluid drag formula
    using Stoke's Law. Particles now have a radius!

--- src/Engine.java
+++ src/Engine.java
@@ -1,10 +1,14 @@
 
 public class Engine
 {
-    final static int MAX_NUM_OF_PARTICLES = 200000;
+    // Fluid viscosities likely to be used
+    public final static double AIR_VISCOSITY = 0.01983d;
+    public final static double WATER_VISCOSITY = 0.001d;
+    
+    public final static int MAX_NUM_OF_PARTICLES = 200000;
     
     Vect3D gravity;
-    double drag;
+    double dragCoefficient;
     
     int NUM_OF_PARTICLES;
     Particle particles[];
@@ -14,15 +18,16 @@ public class Engine
         particles = new Particle[MAX_NUM_OF_PARTICLES];
         NUM_OF_PARTICLES = 0;
         
-        gravity = new Vect3D(0, 0, -9.81d);
-        drag    = 0.5;
+        setGravity(new Vect3D(0d, 0d, -9.81d));
+        setDragCoefficient(AIR_VISCOSITY);
     }
     
     /**
-     * Sets the gravity. It will be directly
-     * added to each component of each particles'
-     * acceleration. The default is Earth's gravity
-     * vector, (0, 0, -9.81)
+     * Sets the gravity as an <b>acceleration</b>,
+     * therefore it will be directly added after the
+     * dynamic forces have been calculated and summed up
+     * together.<br />
+     * Example: for Earth, the vector would be (0, 0, -9.81).
      * 
      * @param gravity
      */
@@ -32,24 +37,16 @@ public class Engine
     }
     
     /**
-     * Sets the coefficient of the linear
-     * drag applied to any moving particle.
-     * It is applied as a force before calculating the
-     * acceleration.
+     * Using <a href="http://en.wikipedia.org/wiki/Stokes%27_law">Stoke's Law</a>,
+     * sets the fluid viscosity that will later be put in the
+     * calculation of the linear drag.
      * 
-     * Should be a number minor than zero, as it is
-     * defined as:
-     * 
-     * Fd = - b*V
-     * 
-     * where V is the velocity vector of the particle
-     * at each updated.
-     * 
-     * @param drag
+     * @param fluidViscosity the viscosity of the fluid the particles
+     * are swimming in, in Pa*s.
      */
-    public void setDragCoefficient(double drag)
+    public void setDragCoefficient(double fluidViscosity)
     {
-        this.drag = drag;
+        this.dragCoefficient = 6*Math.PI*fluidViscosity;
     }
     
     /**
@@ -86,69 +83,87 @@ public class Engine
         }
     }
     
-    private Vect3D getCumulativeForce(Vect3D velocity)
+    private Vect3D getCumulativeForce(Particle p)
     {
-        // TODO: only one for now, drag
         Vect3D force = new Vect3D();
-        
-        force.x = - drag*velocity.x;
-        force.y = - drag*velocity.y;
-        force.z = - drag*velocity.z;
+     
+        // Stoke's Law (fluid drag force)
+        force.x = - dragCoefficient*p.radius*p.vel.x;
+        force.y = - dragCoefficient*p.radius*p.vel.y;
+        force.z = - dragCoefficient*p.radius*p.vel.z;
         
         return force;
     }
     
     /**
-     * Verlet Velocity integration
-     * @param dt
+     * Verlet Velocity integration method,
+     * slightly modified to take into account
+     * forces dependent on velocity (eg, fluid dragCoefficient).
+     * Reference (particle physics paper):
+     * http://pages.csam.montclair.edu/~yecko/ferro/papers/FerroDPD/GrootWarren_ReviewDPD_97.pdf
+     * 
+     * <br /><br />
+     * 
+     * However, as also described here:
+     * http://gamedev.stackexchange.com/a/41917/51181
+     * we used a lambda of 1 and re-arranged a bit to
+     * reduce the number of divisions and multiplications.
+     * Semantically is the same as the above paper.
+     * 
+     * <br /><br />
+     * 
+     * <b>Note: the stackexchange answer is actually wrong in the last
+     * part, where: <br />
+     * velocity += timestep * (newAcceleration - acceleration) / 2;
+     * <br />should be: <br />
+     * velocity += timestep * (acceleration - newAcceleration) / 2;</b><br /><br />
+     * 
+     * <br /><br />
+     * 
+     * Note2: if particles will begin interacting with
+     * each other (attractors for example), this will
+     * need some modifications (updating all positions before
+     * recalculating new accelerations?)
+     * 
+     * @param dt how much to advance the simulation of
      */
     public void update(double dt)
     {
-        double drag = 0.5;
-        
-        int i;
-        
-        Particle p;
-        Vect3D force;
-        Vect3D acc;
-        for (i = 0; i < NUM_OF_PARTICLES; i++)
-        {
-            p = particles[i];
-            acc = p.acc;
-            
-            force = getCumulativeForce(p.vel);
-            
-            acc.x = (force.x / p.mass) + gravity.x;
-            acc.y = (force.y / p.mass) + gravity.y;
-            acc.z = (force.z / p.mass) + gravity.z;
-        }
- 
         // half the delta t, to save
         // a few divisions
         double dt2 = dt / 2d;
         
-        Vect3D pos;
+        Particle p;
+        Vect3D force;
+        Vect3D acc;
         Vect3D vel;
-        for (i = 0; i < NUM_OF_PARTICLES; i++)
+        Vect3D pos;
+        for (int i = 0; i < NUM_OF_PARTICLES; i++)
         {
             p = particles[i];
             pos = p.pos;
             vel = p.vel;
             acc = p.acc;
             
+            force = getCumulativeForce(p);
+            
+            acc.x = (force.x / p.mass) + gravity.x;
+            acc.y = (force.y / p.mass) + gravity.y;
+            acc.z = (force.z / p.mass) + gravity.z;
+            
             pos.x += dt * (vel.x + (dt2 * acc.x));
             pos.y += dt * (vel.y + (dt2 * acc.y));
             pos.z += dt * (vel.z + (dt2 * acc.z));
-        }
-        
-        // TODO: apply forces again as before,
-        // this time without zeroing the acceleration
-        
-        for (i = 0; i < NUM_OF_PARTICLES; i++)
-        {
-            p = particles[i];
-            vel = p.vel;
-            acc = p.acc;
+            
+            vel.x += dt * acc.x;
+            vel.y += dt * acc.y;
+            vel.z += dt * acc.z;
+            
+            force = getCumulativeForce(p);
+            
+            acc.x = -((force.x / p.mass) + gravity.x) + acc.x;
+            acc.y = -((force.y / p.mass) + gravity.y) + acc.y;
+            acc.z = -((force.z / p.mass) + gravity.z) + acc.z;
             
             vel.x += dt2 * acc.x;
             vel.y += dt2 * acc.y;
@@ -164,9 +179,19 @@ public class Engine
         
         Particle p = new Particle();
         p.vel.x = 1d;
-        p.vel.z = 100d;
+        p.vel.y = 0d;
+        p.vel.z = -1d;
+        System.out.println("First particle: initial values");
+        System.out.println("Pos: " + p.pos.x + " " + p.pos.y + " " + p.pos.z);
+        System.out.println("Vel: " + p.vel.x + " " + p.vel.y + " " + p.vel.z);
+        System.out.println("Acc: " + p.acc.x + " " + p.acc.y + " " + p.acc.z);
         w.addParticle(p);
-
+        w.update(STEP);
+        System.out.println("After 1 step (" + STEP + " seconds):");
+        System.out.println("Pos: " + p.pos.x + " " + p.pos.y + " " + p.pos.z);
+        System.out.println("Vel: " + p.vel.x + " " + p.vel.y + " " + p.vel.z);
+        System.out.println("Acc: " + p.acc.x + " " + p.acc.y + " " + p.acc.z);
+        
         long time1 = System.currentTimeMillis();
         System.out.print("Adding particles.. ");
         for (int i = 0; i < 100000; i++)
@@ -178,13 +203,9 @@ public class Engine
         long time2 = System.currentTimeMillis();
         System.out.println((time2 - time1) + " ms");
         
-        System.out.println("Pos: " + p.pos.x + " " + p.pos.y + " " + p.pos.z);
-        System.out.println("Vel: " + p.vel.x + " " + p.vel.y + " " + p.vel.z);
-        System.out.println("Acc: " + p.acc.x + " " + p.acc.y + " " + p.acc.z);
-        
         time1 = System.currentTimeMillis();
         System.out.print("Moving forward the simulation.. ");
-        for (double dt = 0d; dt < 10d; dt = dt + STEP)
+        for (double t = 0d; t < 100d; t = t + STEP)
         {
             w.update(STEP);
         }
@@ -197,7 +218,7 @@ public class Engine
         
         time1 = System.currentTimeMillis();
         System.out.print("Moving forward the simulation.. ");
-        for (double dt = 0d; dt < 10d; dt = dt + STEP)
+        for (double t = 0d; t < 100d; t = t + STEP)
         {
             w.update(STEP);
         }
@@ -210,7 +231,20 @@ public class Engine
         
         time1 = System.currentTimeMillis();
         System.out.print("Moving forward the simulation.. ");
-        for (double dt = 0d; dt < 10d; dt = dt + STEP)
+        for (double t = 0d; t < 100d; t = t + STEP)
+        {
+            w.update(STEP);
+        }
+        time2 = System.currentTimeMillis();
+        System.out.println((time2 - time1) + " ms");
+        
+        System.out.println("Pos: " + p.pos.x + " " + p.pos.y + " " + p.pos.z);
+        System.out.println("Vel: " + p.vel.x + " " + p.vel.y + " " + p.vel.z);
+        System.out.println("Acc: " + p.acc.x + " " + p.acc.y + " " + p.acc.z);
+        
+        time1 = System.currentTimeMillis();
+        System.out.print("Moving forward the simulation.. ");
+        for (double t = 0d; t < 1000d; t = t + STEP)
         {
             w.update(STEP);
         }
--- src/Particle.java
+++ src/Particle.java
@@ -2,6 +2,7 @@
 public class Particle
 {
     double mass;
+    double radius;
     
     Vect3D pos;
     Vect3D vel;
@@ -9,10 +10,21 @@ public class Particle
     
     public Particle()
     {
-        mass = 0d;
+        mass = 1d;
+        radius = 1d;
         
         pos = new Vect3D();
         vel = new Vect3D();
         acc = new Vect3D();
+    }
+    
+    public Particle(double mass, double radius, Vect3D pos, Vect3D vel, Vect3D acc)
+    {
+        this.mass   = mass;
+        this.radius = radius;
+        
+        this.pos = pos;
+        this.vel = vel;
+        this.acc = acc;
     }
 }
